02d3b420e9a7740acaafeb0996ea67e7d584796d
Adam <martia123456789@gmail.com>
2023-12-03 21:12:29 -0600

    Fetching Champs Complete

--- src/models/ChampionInfo.js
+++ src/models/ChampionInfo.js
@@ -1,24 +1,30 @@
 import db from '../helpers/db';
 
 export const getChampions = async (skip, take) => {
-  const count = await db.champion.count();
-  const champions = await db.champion.findMany({
+  const count = await db.Champions.count(); 
+  const champions = await db.Champions.findMany({
     skip,
     take,
   });
   return { count, champions };
-};
+}
+
 
 export const getChampion = async (id) =>
-  db.champion.findUnique({ where: { championId: id } });
+  db.Champions.findUnique({ where: { championId: id } });
+
+
 
 export const addChampion = async (championData) =>
-  db.champion.create({ data: { ...championData } });
+  db.Champions.create({ data: { ...championData } });
+
+
+
 
 export const updateChampion = async (id, championData) => {
   const champion = await getChampion(id);
   if (champion) {
-    return db.champion.update({
+    return db.Champions.update({
       where: { championId: id },
       data: { ...champion, ...championData, updatedAt: new Date() },
     });
@@ -26,5 +32,8 @@ export const updateChampion = async (id, championData) => {
   return null;
 };
 
+
+
+
 export const deleteChampion = async (id) =>
-  db.champion.delete({ where: { championId: id } });
+  db.Champions.delete({ where: { championId: id } });
