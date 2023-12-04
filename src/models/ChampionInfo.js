4d7e2543fc58afa329eaa2b1c6a1341491c62ee8
Adam <martia123456789@gmail.com>
2023-12-03 20:41:18 -0600

    fixed .env

--- src/models/ChampionInfo.js
+++ src/models/ChampionInfo.js
@@ -1,55 +1,36 @@
 import { PrismaClient } from '@prisma/client';
+import db from '../helpers/db'
 
 const prisma = new PrismaClient();
 
-export const addChampion = async (championData) => {
-  try {
-    const newChampion = await prisma.champion.create({
-      data: championData,
-    });
-    return newChampion;
-  } catch (error) {
-    throw new Error('Failed to add champion to the database');
-  }
-};
 
+export const getChampions = async (skip, take) => {
+  const count = await db.Champion.count()
+  const champions = await db.Champion.findMany({
+    skip,
+    take,
+  })
+  return { count, champions }
+}
+
+export const getChampion = async (id) =>
+  db.Champion.findUnique({ where: { championId: id } })
+
+export const addChampion = async (championData) =>
+  db.Champion.create({ data: { ...championData } })
+
+  
 export const updateChampion = async (id, championData) => {
-  try {
-    const updatedChampion = await prisma.champion.update({
+  const champion = await getChampion(id)
+  if (champion) {
+    return db.Champion.update({
       where: { championId: id },
-      data: championData,
-    });
-    return updatedChampion;
-  } catch (error) {
-    throw new Error(`Failed to update champion with ID ${id}: ${error.message}`);
+      data: { ...champion, ...championData, updatedAt: new Date() },
+    })
   }
-};
+  return null
+}
 
-export const deleteChampion = async (id) => {
-  try {
-    const deletedChampion = await prisma.champion.delete({
-      where: { championId: id },
-    });
-    return deletedChampion;
-  } catch (error) {
-    throw new Error(`Failed to delete champion with ID ${id}: ${error.message}`);
-  }
-};
+export const deleteChampion = async (id) =>
+  db.Champion.delete({ where: { championId: id } })
 
-export const getChampions = async (page = 1, size = 10) => {
-  try {
-    const skip = size * (page - 1);
-    const take = size;
-
-    const champions = await prisma.champion.findMany({
-      skip,
-      take,
-    });
-
-    const count = await prisma.champion.count();
-
-    return { count, champions };
-  } catch (error) {
-    throw new Error('Failed to fetch champions from the database');
-  }
-};
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import basicAuth from 'express-basic-auth';
-import ChampionControlRouter from './ChampionControl'; 
+import champions from './ChampionControl'; 
 
 const router = Router();
 
 router.get('/', basicAuth({
-  users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
+  users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASS },
 }), (req, res) => {
   res.send({ msg: 'Welcome to ChampionHub API' }); // Update the welcome message if needed
 });
 
-router.use('/champions', ChampionControlRouter);
+router.use('/champions', champions);
 
 export default router;
